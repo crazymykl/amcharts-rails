2dd3dc575b621afac1a8bbdd82089efac0b47b20
Mike MacDonald (crazymykl) <crazymykl@gmail.com>
2012-09-27 19:08:17 -0400

    skeleton of a rails engine

---
+++ lib/amcharts-rails/version.rb
@@ -0,0 +1,3 @@
+module AmchartsRails
+  VERSION = "0.0.1"
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '1df67d970b15920218aa05b45d3dc44aa78cd8b5197b9492d47ec60f0b63e592638b14d63ce4106c49545b29974f82d3f907da7253dd7b73dea216b4f9abf87a'
